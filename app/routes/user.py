d8037a1f8b74468e2e2a58ecc6567133b8ae5441
Paras Nath <paras94nitjsr@gmail.com>
2026-01-22 19:47:20 +0530

    modified the code

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,10 +1,31 @@
-from fastapi import APIRouter
-from app.domain.user import User
-from app.storage.user_store import save_user
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-router = APIRouter(prefix="/user", tags=["User"])
+from app.database import get_db
+from app.models.user import User
+from app.models.emergency_content import EmergencyContact
+
+router = APIRouter()   # ✅ NO PREFIX HERE
 
 @router.post("/register")
-def register_user(user: User):
-    save_user(user)
-    return {"status": "registered"}
+def register_user(payload: dict, db: Session = Depends(get_db)):
+    user_id = payload["user_id"]
+    email = payload["email"]
+    emergency_phone = payload["emergency_contact"]["phone"]
+
+    user = User(
+        id=user_id,
+        email=email,
+        phone=user_id
+    )
+    db.add(user)
+
+    contact = EmergencyContact(
+        user_id=user_id,
+        phone=emergency_phone
+    )
+    db.add(contact)
+
+    db.commit()
+
+    return {"status": "user registered"}
